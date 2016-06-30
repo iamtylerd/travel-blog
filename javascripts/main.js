9b2a694cba87febe38f1683b6239b2dbf1dd1d12
iamtylerd <tylerdanielphotography@gmail.com>
2016-06-30 11:34:58 -0500

    new

---
+++ javascripts/main.js
@@ -0,0 +1,9 @@
+// INSTAFEED
+    var feed = new Instafeed({
+        get: 'user',
+        userId: '2660000221',
+        accessToken: '2660000221.e546faa.4c478924aa644469b848d7a6aa5541ca',
+        links: 'true',
+        limit: '9',
+    });
+    feed.run();
