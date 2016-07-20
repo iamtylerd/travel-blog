afcce5ef27b10bd813e2df409e673ebb59e58af1
iamtylerd <tylerdanielphotography@gmail.com>
2016-07-20 14:53:04 -0500

    first commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,7 @@
         get: 'user',
         userId: '2660000221',
         accessToken: '2660000221.e546faa.4c478924aa644469b848d7a6aa5541ca',
-        links: 'true',
+        links: true,
         limit: '9',
     });
     feed.run();
